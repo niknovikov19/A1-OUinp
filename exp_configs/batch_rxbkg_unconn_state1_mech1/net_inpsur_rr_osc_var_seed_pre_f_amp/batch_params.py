edaf1e9b6f695db96309769003ae696611e5e62d
niknovikov19 <nikknovikov@gmail.com>
2026-06-10 17:32:05 -0400

    Create exp for r->r transfer coef probing

---
+++ exp_configs/batch_rxbkg_unconn_state1_mech1/net_inpsur_rr_osc_var_seed_pre_f_amp/batch_params.py
@@ -0,0 +1,60 @@
+import numpy as np
+
+
+PYR_POPS = ['IT2', 'IT3', 'ITP4', 'ITS4', 'IT5A', 'IT5B', 'IT6',
+            'CT5A', 'CT5B', 'CT6', 'PT5B']
+PV_POPS = ['PV2', 'PV3', 'PV4', 'PV5A', 'PV5B', 'PV6']
+SOM_POPS = ['SOM2', 'SOM3', 'SOM4', 'SOM5A', 'SOM5B', 'SOM6']
+VIP_POPS = ['VIP2', 'VIP3', 'VIP4', 'VIP5A', 'VIP5B', 'VIP6']
+NGF_POPS = ['NGF1', 'NGF2', 'NGF3', 'NGF4', 'NGF5A', 'NGF5B', 'NGF6']
+
+L2_POPS = ['IT2', 'PV2', 'SOM2', 'VIP2', 'NGF2']
+L4_POPS = ['ITP4', 'ITS4', 'PV4', 'SOM4', 'VIP4', 'NGF4']
+CTX_POPS = PYR_POPS + PV_POPS + SOM_POPS + VIP_POPS + NGF_POPS
+
+THAL_E_POPS = ['TC', 'HTC', 'TCM']
+THAL_I_POPS = ['TI', 'TIM', 'IRE', 'IREM']
+CORE_POPS = ['TC', 'HTC', 'TI', 'IRE']
+MATX_POPS = ['TCM', 'TIM', 'IREM']
+THAL_POPS = CORE_POPS + MATX_POPS
+
+
+N_SEEDS = 5
+POPS_PRE = L2_POPS
+OSC_F_VALUES = [5]
+OSC_AMP_VALUES = np.geomspace(0.01, 0.2, 10).round(3).tolist()
+
+
+def get_batch_params():
+    """Generate params for batchtools to probe."""
+    params = {
+        'seed_main': (1000 + np.arange(N_SEEDS)).tolist(),
+        'pop_pre': POPS_PRE,
+        'osc_f': OSC_F_VALUES,
+        'osc_amp': OSC_AMP_VALUES,
+    }
+    return params
+
+
+def post_update(cfg):
+    """Called after cfg.update()."""
+
+    cfg.seeds['stim'] = cfg.seed_main
+    cfg.seeds['conn'] = cfg.seed_main * 2
+    cfg.subnet_params['global_seed'] = cfg.seed_main * 3
+
+    for n, pop in enumerate(cfg.bkg_spike_inputs):
+        cfg.bkg_spike_inputs[pop]['exc']['seed'] = (
+            cfg.seeds['stim'] + 10000 + n
+        )
+        cfg.bkg_spike_inputs[pop]['inh']['seed'] = (
+            cfg.seeds['stim'] + 20000 + n
+        )
+
+    cfg.batch_par_info = {
+        'n_seeds': N_SEEDS,
+        'pops_pre': POPS_PRE,
+        'osc_f_values': OSC_F_VALUES,
+        'osc_amp_values': OSC_AMP_VALUES,
+        'batch_params': ['seed_main', 'pop_pre', 'osc_f', 'osc_amp'],
+    }
